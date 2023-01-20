a77bc87205762e48584f0f34dd0c4dad094226d9
Dan Kadera <me@subsonic.cz>
2023-01-20 22:07:29 +0100

    export helpers

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './buffer';
 export * from './decoder';
 export * from './encoder';
 export * from './eventEmitter';
+export * from './helpers';
 export * from './types';
 export * from './values';
